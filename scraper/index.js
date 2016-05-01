798afe21cda02b9ea6dad6aaa6a34944f87af511
sam <samuel.j.simmons@gmail.com>
2016-05-01 22:01:30 +1200

    add html string of all matches

--- scraper/index.js
+++ scraper/index.js
@@ -1,7 +1,9 @@
-let cheerio = require('cheerio')
-let request = require('superagent')
+const cheerio = require('cheerio')
+const request = require('superagent')
+const fs = require('fs')
 
 let seasonURL = "http://www.espncricinfo.com/georgie-pie-super-smash-2015-16/engine/series/917513.html"
+let data = []
 
 request.get(seasonURL)
   .end((err, res) => {
@@ -15,5 +17,20 @@ request.get(seasonURL)
         results.push(link.attribs.href)
       }
     })
-    console.log(results)
+    results.forEach((link) => {
+      request.get('http://www.espncricinfo.com' + link)
+        .end((err, res) => {
+          if (err) {console.error(err)}
+
+          // console.log(res.text)
+          let $ = cheerio.load(res.text)
+          let tableHtml = $('body').html()
+          data.push(tableHtml)
+          fs.writeFile('data.json', JSON.stringify(data), (err) => {
+            if (err) {console.error(err)}
+
+            console.log('file written')
+          })
+        })
+    })
   })
