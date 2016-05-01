d6a41cdc54e1e6487b6e53c6af32ff977ca4c72a
sam <samuel.j.simmons@gmail.com>
2016-05-02 11:56:19 +1200

    first run at data wrangling done, saved to filteredData.json

--- scraper/gameScrape.js
+++ scraper/gameScrape.js
@@ -8,13 +8,27 @@ fs.readFile('data.json', (err, data) => {
   let $ = cheerio.load(data.toString())
   let rows = $('table').find('td').toArray()
   rows.forEach((tableData) => {
-    if (tableData.children.length > 1 ) {
+    if (tableData.children.length > 1) {
       if (tableData.children[0].name === 'a'){
         results.push(tableData.children[0].children[0].data)
       }
     } else if (tableData.children.length > 0) {
-        results.push(tableData.children[0].data)
+        if (tableData.children[0].name === 'a' || tableData.children[0].name === 'b'){
+          results.push(tableData.children[0].children[0].data)
+        } else {
+          if (tableData.children[0].type === 'text'){
+            results.push(tableData.children[0].data)
+          }
+        }
     }
   })
-  console.log(results)
+  console.log(results.length)
+  results = results.filter(x => {
+    if (x)
+      return true
+  })
+  fs.writeFile('filteredData.json', JSON.stringify(results), (err) => {
+    if (err) {console.error(err)}
+    console.log('results have been written to file.')
+  })
 })
---
+++ scraper/test.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+fs.readFile('filteredData.json', (err, data) => {
+  if (err) {console.error(err)}
+
+  data = JSON.parse(data).map(entry => entry.trim())
+  console.log(data.length)
+})
