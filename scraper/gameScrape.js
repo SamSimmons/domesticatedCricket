63a818b480c4bba853b0b01b3f7d627180177c74
sam <samuel.j.simmons@gmail.com>
2016-05-02 22:57:27 +1200

    replace lodash with ramda

--- scraper/gameScrape.js
+++ scraper/gameScrape.js
@@ -1,11 +1,20 @@
 const cheerio = require('cheerio')
 const fs = require('fs')
-const _ = require('lodash')
+const R = require('ramda')
 
-let results = []
 fs.readFile('season.json', (err, data) => {
   if (err) {console.error(err)}
   data = JSON.parse(data)
-  console.log(data.length)
+  let mapped = data.map((game) => {
+    console.log(R.splitEvery(8, game.batting))
+    return {
+      link: game.link,
+      batting: R.splitEvery(8, game.batting),
+      bowling: R.splitWhen(R.equals(' '), game.bowling)
+    }
+    // game.batting = R.splitEvery(8, game.batting)
+    // game.bowling = R.splitWhen(R.equals(' '), game.bowling)
+  })
+  console.log(mapped[0].batting)
 
 })
