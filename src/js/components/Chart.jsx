7e2b461a757dfaac5e6b9c31452c28370c5fe774
sam <samuel.j.simmons@gmail.com>
2016-05-05 11:06:46 +1200

    add-chart

--- src/js/components/Chart.jsx
+++ src/js/components/Chart.jsx
@@ -4,26 +4,88 @@ import d3 from 'd3'
 export default class Chart extends Component {
   constructor(props) {
     super(props)
-    this.createLineGraph()
+    // this.createLineGraph()
   }
 
-  createLineGraph(){
+  componentDidMount() {
+    let data = [
+      {game: 0, runs: 2},
+      {game: 1, runs: 15},
+      {game: 2, runs: 80},
+      {game: 3, runs: 60}
+    ]
+    // this.plotData("RJ Nicol")
+    this.createLineGraph(data)
+  }
+// TODO use find here and find the player data and return it if it matches
+  // plotData(name) {
+  //   // console.log(this.props.data)
+  //   this.props.data.map(game => {
+  //     runsgame.map(entry => {
+  //       if ( entry.batsman === name ){
+  //         return parseInt(entry.runs)
+  //       }
+  //     })
+  //   })
+  // }
+
+  createLineGraph(data){
     console.log('createLine')
-    let x = d3.scale.linear().range([0, 1000])
-    let y = d3.scale.linear().range([0, 500])
+    const margin = {top: 20, right: 20, bottom: 30, left: 50}
+    const width = 960 - margin.left - margin.right
+    const height = 500 - margin.top - margin.bottom
 
-    let xAxis = d3.svg.axis().scale(x)
-      .orient('bottom').ticks(this.props.data.length)
+    let x = d3.scale.linear()
+        .range([0, width])
 
-    let yAxis = d3.scg.axis().scale(y)
-      .orient('left').ticks(5)
+    let y = d3.scale.linear()
+        .range([height, 0])
+
+    let xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+
+    let yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+
+    let line = d3.svg.line()
+      .x(function(d) { return x(d.game); })
+      .y(function(d) { return y(d.runs); })
+
+
+    let svg = d3.select(".chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+    x.domain(d3.extent(data, function(d) { return d.game; }))
+    y.domain(d3.extent(data, function(d) { return d.runs; }))
+
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+
+
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+
   }
 
   render() {
-    console.log('props', this.props)
+    // console.log('props', this.props)
     return (
       <div className="chart">
-        <svg width='1000' height='500'></svg>
+
       </div>
     )
   }
