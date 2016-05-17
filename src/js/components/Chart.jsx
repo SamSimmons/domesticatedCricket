654b3077df173d1c094f68544799ffbf3f321091
sam <samuel.j.simmons@gmail.com>
2016-05-17 22:47:04 +1200

    begin refactor d3 code to show line graphs

--- src/js/components/Chart.jsx
+++ src/js/components/Chart.jsx
@@ -16,51 +16,25 @@ export default class Chart extends Component {
   }
 
   createLineGraph(data){
-    const margin = {top: 20, right: 20, bottom: 30, left: 50}
-    const width = 960 - margin.left - margin.right
-    const height = 500 - margin.top - margin.bottom
+    console.log(data)
+    const svg = d3.select('.chart').append('svg')
+      .attr({
+        width: '800px',
+        height: '500px'
+      })
 
-    let x = d3.scale.linear()
-        .range([0, width])
+    const mapLine = d3.svg.line()
+      .x((d, i) => (800 / data.length) * i)
+      .y(d => 500 - d.runs)
+      .interpolate('linear')
 
-    let y = d3.scale.linear()
-        .range([height, 0])
-
-    let xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom")
-
-    let yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left")
-
-    let line = d3.svg.line()
-      .x(function(d, i) { return x(i); })
-      .y(function(d) { return y(d.runs); })
-
-
-    let svg = d3.select(".chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-    x.domain(d3.extent(data, (d, i) => i ))
-    y.domain(d3.extent(data, d => d.runs))
-
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-
-    svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
+    const vis = svg.append('path')
+      .attr({
+        d: mapLine(data),
+        "stroke": "steelblue",
+        "stroke-width": 2,
+        "fill": "none"
+      })
   }
 
 
