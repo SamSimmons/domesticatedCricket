f7ce51457d9fd55fb2ad9d0f72603aaa3ad9f354
sam <samuel.j.simmons@gmail.com>
2016-05-19 23:25:33 +1200

    add moving buttons for switching views on graph

--- src/js/components/Chart.jsx
+++ src/js/components/Chart.jsx
@@ -4,25 +4,40 @@ import R from 'ramda'
 import { createLineGraph } from '../graph'
 
 export default class Chart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      graph: 'totals'
+    }
+  }
 
   componentDidMount() {
     createLineGraph(this.props.data, this.props.selectedPlayer)
   }
 
-// TODO use find here and find the player data and return it if it matches
+  handleRunsPer() {
+    this.setState({graph: "runsPer" })
+    this.updateGraph()
+  }
 
+  handleTotals() {
+    this.setState({graph: 'totals'})
+    this.updateGraph()
+  }
 
   updateGraph(){
     d3.select('svg').remove()
-    createLineGraph(this.props.data, this.props.selectedPlayer)
+    createLineGraph(this.props.data, this.props.selectedPlayer, this.state.graph)
   }
-
 
   render() {
     this.updateGraph()
     return (
-      <div className="chart" onClick={this.props.handleClick.bind(this)}>
-
+      <div className="chart-wrapper">
+        <button onClick={this.handleTotals.bind(this)}>Total runs in season</button>
+        <button onClick={this.handleRunsPer.bind(this)}>Runs per game</button>
+        <div className="chart" onClick={this.props.handleClick.bind(this)}>
+        </div>
       </div>
     )
   }
--- src/js/graph.js
+++ src/js/graph.js
@@ -5,6 +5,18 @@ function reduceForUniquePlayers(games){
   return R.compose(R.uniq, R.map(x => x.batsman), R.flatten)(games)
 }
 
+function findMaxSeasonRuns(data, players) {
+  return players.map(player => getPlayersSeason(player, data)
+    .reduce((a,b) => a + parseInt(b.runs), 0))
+      .reduce((a, b) => R.max(a, b))
+}
+
+function findMaxAverage(data, players) {
+  let playersMovingAverages = players.map(player => getRunsPer(getPlayersSeason(player, data)))
+  let playersHighest = playersMovingAverages.map(x => x.reduce((a,b) => R.max(a, b.runs), 0))
+  return playersHighest.reduce((a,b) => R.max(a, b))
+}
+
 function getPlayersSeason(name, data) {
   return data.map(game => {
     return game.filter(entry => (name === entry.batsman))
@@ -12,7 +24,6 @@ function getPlayersSeason(name, data) {
 }
 
 function getRunningTotals(data) {
-  console.log(data)
   return data.map(x => {
     return {
       'batsman': x.batsman,
@@ -28,31 +39,34 @@ function getRunningTotals(data) {
   }, [])
 }
 
+function getRunsPer(data) {
+  return data.map((current, i) => {
+    if (i > 0 ){
+      return {
+        'batsman': current.batsman,
+        'runs': R.mean(data.slice(0, i).map(x => x.runs))
+      }
+    }
+    return null
+  }).filter(x => x !== null)
+}
 
-function createLineGraph(data, selectedPlayer){
 
+function createLineGraph(data, selectedPlayer, type = "totals"){
   function drawLine (data) {
-    if (data[0].batsman === selectedPlayer) {
-      svg.append('path')
-        .attr({
-          d: line(data),
-          'stroke': 'steelblue',
-          'stroke-width': 7,
-          'fill': 'none',
-          'class': 'selectedPlayer',
-          'id': data[0].batsman
-        });
-
-    } else {
+      if (data.length < 1) {
+        return
+      }
+      let possibleSelected = (selectedPlayer === data[0].batsman) ? 'selectedPlayer' : ''
       svg.append('path')
         .attr({
           d: line(data),
           'stroke': 'grey',
           'stroke-width': 5,
           'fill': 'none',
-          'id': data[0].batsman
-        });
-      }
+          'id': data[0].batsman,
+          'class': possibleSelected
+        })
     }
 
   const players = reduceForUniquePlayers(data)
@@ -63,7 +77,10 @@ function createLineGraph(data, selectedPlayer){
 
   const x = d3.scale.linear().range([0, width])
   const y = d3.scale.linear().range([height, 0])
-  const max = R.flatten(data).map(x => x.runs).reduce((x, y) => (parseInt(x) > parseInt(y)) ? x : y)
+  let max = findMaxSeasonRuns(data, players)
+  if (type === 'runsPer') {
+    max = findMaxAverage(data, players)
+  }
   x.domain([0, data.length])
   y.domain([0, max])
 
@@ -101,9 +118,11 @@ function createLineGraph(data, selectedPlayer){
       // .attr('transform', 'translate(' + width + ',0)')
       .call(yAxis);
 
-    console.log(getRunningTotals(getPlayersSeason(selectedPlayer, data)))
-    players.forEach(player => drawLine(getRunningTotals(getPlayersSeason(player, data))))
-
+    if (type === 'runsPer'){
+      players.forEach(player => drawLine(getRunsPer(getPlayersSeason(player, data))))
+    } else {
+      players.forEach(player => drawLine(getRunningTotals(getPlayersSeason(player, data))))
+    }
 }
 
 export {
